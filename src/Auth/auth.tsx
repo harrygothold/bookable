2dfdd695f2707cdee145c37d854754143d754ee3
Caleb Whittington <calebwhitt@outlook.com>
2020-04-15 16:31:48 +0100

    added styling for the landing page. may potentially add a background-image for the hero

--- src/Auth/auth.tsx
+++ src/Auth/auth.tsx
@@ -1,54 +1,77 @@
-import React, { FC, useState, ChangeEvent, FormEvent } from "react";
-import { useHistory } from "react-router-dom";
-import { Auth } from "aws-amplify";
+import React, { FC } from 'react';
+import './auth.css'
 
-const SignInContainer: FC = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const history = useHistory();
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    const { username, password } = formData;
-    e.preventDefault();
-    Auth.signUp({
-      username,
-      password,
-    }).then(() => {
-      history.push(`/confirmation?email=${username}`);
-    });
-  };
-
+const LandingPage: FC = () => {
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <h1>Sign Up</h1>
-      <input
-        name="username"
-        onChange={(e) => handleChange(e)}
-        value={formData.username}
-        type="email"
-        placeholder="Email"
-      />
-      <input
-        name="password"
-        onChange={(e) => handleChange(e)}
-        value={formData.password}
-        type="password"
-        placeholder="Password"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+      <div className='hero'>
+        <div className='central'>
+          <div className='top'>
+            <h1>Bookable</h1>
+          </div>
 
-export default SignInContainer;
+          <div className='bottom'>
+            <div className='split'>
+              LOG IN
+            </div>
+            <div className='split'>
+              SIGN UP
+            </div>
+          </div>
+        </div>
+      </div>
+
+  )
+}
+
+export default LandingPage;
+
+// const SignInContainer: FC = () => {
+//   const [formData, setFormData] = useState({
+//     username: "",
+//     password: "",
+//   });
+//
+//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     const { value, name } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
+//
+//   const history = useHistory();
+//
+//   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+//     const { username, password } = formData;
+//     e.preventDefault();
+//     Auth.signUp({
+//       username,
+//       password,
+//     }).then(() => {
+//       history.push(`/confirmation?email=${username}`);
+//     });
+//   };
+//
+//   return (
+//     <form onSubmit={(e) => handleSubmit(e)}>
+//       <h1>Sign Up</h1>
+//       <input
+//         name="username"
+//         onChange={(e) => handleChange(e)}
+//         value={formData.username}
+//         type="email"
+//         placeholder="Email"
+//       />
+//       <input
+//         name="password"
+//         onChange={(e) => handleChange(e)}
+//         value={formData.password}
+//         type="password"
+//         placeholder="Password"
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// };
+//
+// export default SignInContainer;
